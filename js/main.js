72cec33d10ec11ede59f8418aa652123d6872dde
mburbach <mail@martin-burbach>
2016-10-26 18:36:47 +0200

    Like a native App
    + namedconfig.xml for better build options
    + Feature: add notification for App

--- js/main.js
+++ js/main.js
@@ -14,10 +14,21 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new LocalStorageStore();
+    	var self = this;
+        this.store = new LocalStorageStore( function() {
+        	self.showAlert('Store initialized', 'Info')
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+    
 };
 
 app.initialize();
